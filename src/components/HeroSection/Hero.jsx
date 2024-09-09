847ed6ee8af5fd31a93add00684c051546c9bfdb
Korede Anunusor <koredeanunusor@gmai.com>
2024-09-09 11:52:10 +0100

    Postioning

---
+++ src/components/HeroSection/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navbar } from '../NavBar/Navbar'
+import "./hero.css"
+
+const Hero = (props) => {
+  return (
+    <>
+
+    <div className="hero" style={{
+        backgroundImage: `url(${props.background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+    }}>
+        <Navbar/>
+        <div className="heroText">
+            <h2 className='theme'>{props.theme}</h2>
+            <p className='content'>{props.content}</p>
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default Hero
