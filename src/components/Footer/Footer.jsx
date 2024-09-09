a244a95c2661d0088ea4264c971cdd58961be0f2
Korede Anunusor <koredeanunusor@gmai.com>
2024-09-09 14:24:58 +0100

    Updated Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,53 +1,55 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import { Link } from "react-router-dom";
 import { FaFacebookF } from "react-icons/fa";
 import { LuInstagram } from "react-icons/lu";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { BiLogoMediumOld } from "react-icons/bi";
 
-
 const Footer = () => {
   return (
     <>
-
-    <div className="footerDiv">
+      <div className="footerDiv">
         <div className="midDiv">
-        <div className="mid">
-           <div className="footerlogo"></div>
-        </div>
-        <div className="mid">
-            <Link>Home</Link>
-            <Link>Services</Link>
-            <Link>Themes</Link>
-        </div>
-        <div className="mid">
-            <Link>Support</Link>
-            <Link>Showcase</Link>
-            <Link>Get in Touch</Link>
-            <Link>Connect</Link>
-        </div>
-        <div className="mid">
-            <Link>Our Team</Link>
-            <Link>About us</Link>
-            <Link>Contact us</Link>
-            <Link>Need Help?</Link>
-            <div className="icon">
-                <FaFacebookF />
-                <LuInstagram />
-                <FaXTwitter />
-                <FaLinkedinIn />
-                <BiLogoMediumOld />
+          <div className="mid">
+            <div className="footerlogo"
+            style={{fontWeight: "bold"}}>TechWiz
             </div>
-        </div>
+          </div>
+          <div className="mid">
+            <Link to="/">Home</Link>
+            <Link to="/service">Services</Link>
+            <Link to="/">Themes</Link>
+          </div>
+          <div className="mid">
+            <Link to="/support">Support</Link>
+            <Link to="/show">Showcase</Link>
+            <Link to="/contactUs">Get in Touch</Link>
+            <Link to="/contactUs">Connect</Link>
+          </div>
+          <div className="mid">
+            <Link to="/aboutUs">Our Team</Link>
+            <Link to="/aboutUs">About us</Link>
+            <Link to="/contactUs">Contact us</Link>
+            <Link to="/aboutUs">Need Help?</Link>
+            <div className="icon">
+              <FaFacebookF />
+              <LuInstagram />
+              <FaXTwitter />
+              <FaLinkedinIn />
+              <BiLogoMediumOld />
+            </div>
+          </div>
         </div>
         <div className="last">
-            <p>2024 &copy;Copyright; Designed by <span className='alpha'>Alpha Coders</span></p>
+          <p>
+            2024 &copy;Copyright; Designed by Alpha Coders
+          </p>
         </div>
-    </div>
-      
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
