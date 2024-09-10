92dc8d6767d7ce951b513c2e168552e7d452b2c0
Korede Anunusor <koredeanunusor@gmai.com>
2024-09-10 21:33:26 +0100

    About us

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import "./about.css"
+// import { Hero } from '../../components/HeroSection/Hero'
+import { Navbar } from '../../components/NavBar/Navbar'
+// import About from '../../assets'
+
+const About = () => {
+  return (
+    <>
+
+    <div className="aboutDiv">
+    <Navbar/>
+        {/* <Hero theme="About Us" content="Learn more about us"/> */}
+        <div className="best">
+            <div className="img">
+                {/* <img src={About} alt="" /> */}
+            </div>
+            <div className="aboutText">
+                <h4>Our Company</h4>
+                <h2>We always give the <span className='h2'>best</span>  experience</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, illo inventore excepturi fuga, quam delectus earum nulla velit tenetur doloribus qui voluptate nam hic nesciunt? Veritatis corporis similique sit voluptatibus!</p>
+                <button className='button'><h4>Contact Us Now</h4></button>
+            </div>
+        </div>
+        <div className="best">
+            <div className="leftWhat">
+            <h4>What we do</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis corporis tempora dolorum deleniti. Doloremque quibusdam inventore quo, distinctio a in esse dolorem omnis provident quas modi? Suscipit porro eum distinctio?</p>
+            <h5>Lorem ipsum dolor sit amet.</h5>
+            <h5>Lorem ipsum dolor sit amet.</h5>
+            <h5>Lorem ipsum dolor sit amet.</h5>
+            <h5>Lorem ipsum dolor sit amet.</h5>
+            </div>
+           
+           <div className="rightWhat">
+              <div className="right">
+                <h3>2000+</h3>
+                <p>Reliable Employees</p>
+              </div>
+              <div className="right">
+                <h3>3800+</h3>
+                <p>Satisfied Clients</p>
+              </div>
+              <div className="right">
+                <h3>1000+</h3>
+                <p>Completed Projects</p>
+              </div>
+              <div className="right">
+                <h3>50</h3>
+                <p>Well-deserved awards</p>
+              </div>
+           </div>
+        </div>
+        <div className="why"></div>
+    </div>
+      
+    </>
+  )
+}
+
+export default About
